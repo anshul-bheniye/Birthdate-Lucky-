a0cce0febb5e0f93722f431fb1896b7d995edd90
Anshul Bheniye <1.anshulbheniye@gmail.com>
2021-08-30 10:52:02 +0530

    completed

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-alert('Virtual Handshake 100%');
+let dateOfBirth = document.querySelector('.card .dob');
+let luckyNumber = document.querySelector('.card .luckyNumber');
+let displayMessage = document.querySelector('.container .answer');
+
+
+
+// button onclick
+let button = document.querySelector('.card .btn');
+
+button.addEventListener('click', function(e){
+
+// preventing default behaviour of button
+    e.preventDefault();
+
+// selecting and adding full date
+     let x = dateOfBirth.value;
+     let y = new Date(x);
+     let date = y.getDate();
+     let month = y.getMonth();
+     let year = y.getFullYear();
+
+     let sumOfDMY = date + month + year;
+
+// selecting lucky Number
+     let fetchLN = luckyNumber.value;
+
+// display message
+ if(sumOfDMY % fetchLN === 0){
+     displayMessage.innerText=`${fetchLN} is lucky number 🎊🎊`;
+    } 
+    else if(fetchLN === "" || sumOfDMY === undefined){
+        displayMessage.innerText='Fill up both fields';
+    }
+ else{
+    displayMessage.innerText=`${fetchLN} is not that lucky 😞`;
+    }
+
+   
+});
